18d342e08ddfa06e6a7ce70ce9fbb66c8e7e60f5
Tomas Ortega <tomasorpa@gmail.com>
2024-07-26 12:02:41 -0400

    finishing project

---
+++ app.js
@@ -0,0 +1,5 @@
+let enlacesHeader = document.querySelectorAll(".div__links")[0];
+
+document.querySelectorAll("#burger")[0].addEventListener("click", function () {
+  enlacesHeader.classList.toggle("menudos");
+});
